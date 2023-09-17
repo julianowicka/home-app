f1678b67cbd883531c5e34c818840c125af6311a
julia nowicka <julia.nowicka@lemondev.pl>
2023-09-17 20:57:18 +0200

    Add details about apartments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,17 @@ import { RouterModule } from '@angular/router';
     RouterModule,
   ],
   template: `
-    <main>
+  <main>
+    <a [routerLink]="['/']">
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
