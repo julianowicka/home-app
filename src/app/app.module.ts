7b48961c72b745cceb4dfd359db118569eba3f7e
julia nowicka <julia.nowicka@lemondev.pl>
2023-09-17 17:15:04 +0200

    Add home component

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AppComponent
+
+  ],
+  providers: [],
+})
+export class AppModule { }
