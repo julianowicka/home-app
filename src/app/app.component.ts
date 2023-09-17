1f77e4c3a920e672135437ac145084831180af0d
julia nowicka <julia.nowicka@lemondev.pl>
2023-09-17 20:22:34 +0200

    Add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HomeComponent,
+    RouterModule,
   ],
   template: `
     <main>
